8c3b6004bc1d99b5f61623d17f29d428a8330a88
Dev Tomar <devtomardev735@gmail.com>
2026-03-07 12:02:01 +0530

    fix indentation error

--- app.py
+++ app.py
@@ -63,8 +63,7 @@ def download():
     pdf.build(elements)
 
     return send_file(file_path, as_attachment=True)
-
-    if __name__ == "__main__":
+if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
